963b4447f92ee08ab3d55bc42974e0ff49d7c7bb
John Flynn <jflynnpics@gmail.com>
2018-11-06 14:05:51 -0500

    Done with explorer mode

--- public/main.js
+++ public/main.js
@@ -1,5 +1,36 @@
+const currentCity = () => {
+  return document.getElementById('place').value
+}
+
+const doSearch = event => {
+  event.preventDefault()
+
+  let cityURL
+  if (isNaN(currentCity())) {
+    cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity()}&units=imperial&APPID=1dea439c0612bd850d27d1cfda4844cc`
+  } else {
+    cityURL = `https://api.openweathermap.org/data/2.5/weather?zip=${currentCity()}&units=imperial&APPID=1dea439c0612bd850d27d1cfda4844cc`
+  }
+
+  let weather = fetch(cityURL)
+    .then(response => {
+      return response.json()
+    })
+    .then(weather => {
+      newLI = document.createElement('li')
+      document.querySelector('.conditions').appendChild(newLI)
+      newLI.textContent = `Temperature: ${weather.main.temp}°F`
+      newLI2 = document.createElement('li')
+      document.querySelector('.conditions').appendChild(newLI2)
+      newLI2.textContent = `Humidity: ${weather.main.humidity} %`
+      newLI3 = document.createElement('li')
+      document.querySelector('.conditions').appendChild(newLI3)
+      newLI3.textContent = `Wind: ${weather.wind.speed} MPH`
+    })
+}
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.querySelector('.search').addEventListener('click', doSearch)
 }
 
 document.addEventListener('DOMContentLoaded', main)
